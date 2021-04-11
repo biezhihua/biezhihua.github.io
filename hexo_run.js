08243f4e40135f57ddb68e9781c16be8da28e970
biezhihua <biezhihua@gmail.com>
2021-04-11 11:25:05 +0800

    feat: hexo run js

---
+++ hexo_run.js
@@ -0,0 +1,10 @@
+const {exec} = require('child_process')
+
+exec('hexo server', (error, stdout, stderr) => {
+	if (error) {
+		console.log('exec error: ${error}');
+		return;
+	}
+	console.log('stdout: ${stdout}');
+	console.log('stderr: ${stderr}');
+})
